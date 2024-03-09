25bc099e11f62e55d22b9245458d6c74aadb62b0
mendoza000 <omarmendoza.alv25@gmail.com>
2024-03-09 11:00:21 -0400

    clean page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
-
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24">
-			<Button>Prueba</Button>
-		</main>
+		<main className="flex flex-col items-center justify-center p-24"></main>
 	)
 }
