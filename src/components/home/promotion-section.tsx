85cb2d6c840224fb80f38a208f6a185e304eebcb
mendoza000 <omarmendoza.alv25@gmail.com>
2024-03-15 11:27:19 -0400

    add previous and next

--- src/components/home/promotion-section.tsx
+++ src/components/home/promotion-section.tsx
@@ -1,56 +1,88 @@
 import SectionTitle from "../ui/section-title"
 import ProductCard from "../ui/product-card"
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "../ui/carousel"
 
 export default function PromotionSection() {
 	return (
 		<section className="max-w-7xl mx-auto px-5 mt-5 pb-10">
 			<SectionTitle title="Promociones rápidas" subtitle="Hoy" />
-			<div className="mt-7 grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-5">
-				<ProductCard
-					promotion
-					price={50}
-					oldPrice={70}
-					title="Nike Dunk"
-					imageAlt="nike-dunk"
-					imageURL="/images/dunk.jpg"
-				/>
 
-				<ProductCard
-					promotion
-					price={60}
-					oldPrice={90}
-					title="Air Max"
-					imageAlt="air-max"
-					imageURL="/images/airmax.jpg"
-				/>
+			<Carousel className="mt-7">
+				<CarouselPrevious className="hidden lg:flex" />
+				<CarouselNext className="hidden lg:flex" />
 
-				<ProductCard
-					promotion
-					price={50}
-					oldPrice={70}
-					title="Nike AJKO"
-					imageAlt="nike-ajko"
-					imageURL="/images/ajko.jpg"
-				/>
+				<CarouselContent>
+					<CarouselItem className="basis-1/1 md:basis-1/3 lg:basis-1/5">
+						<ProductCard
+							promotion
+							price={50}
+							oldPrice={70}
+							title="Nike Dunk"
+							imageAlt="nike-dunk"
+							imageURL="/images/dunk.jpg"
+						/>
+					</CarouselItem>
+					<CarouselItem className="basis-1/1 md:basis-1/3 lg:basis-1/5">
+						<ProductCard
+							promotion
+							price={60}
+							oldPrice={90}
+							title="Air Max"
+							imageAlt="air-max"
+							imageURL="/images/airmax.jpg"
+						/>
+					</CarouselItem>
 
-				<ProductCard
-					promotion
-					price={50}
-					oldPrice={70}
-					title="Nike Dunk"
-					imageAlt="nike-dunk"
-					imageURL="/images/dunk.jpg"
-				/>
+					<CarouselItem className="basis-1/1 md:basis-1/3 lg:basis-1/5">
+						<ProductCard
+							promotion
+							price={50}
+							oldPrice={70}
+							title="Nike AJKO"
+							imageAlt="nike-ajko"
+							imageURL="/images/ajko.jpg"
+						/>
+					</CarouselItem>
 
-				<ProductCard
-					promotion
-					price={60}
-					oldPrice={90}
-					title="Air Max"
-					imageAlt="air-max"
-					imageURL="/images/airmax.jpg"
-				/>
-			</div>
+					<CarouselItem className="basis-1/1 md:basis-1/3 lg:basis-1/5">
+						<ProductCard
+							promotion
+							price={50}
+							oldPrice={70}
+							title="Nike Dunk"
+							imageAlt="nike-dunk"
+							imageURL="/images/dunk.jpg"
+						/>
+					</CarouselItem>
+					<CarouselItem className="basis-1/1 md:basis-1/3 lg:basis-1/5">
+						<ProductCard
+							promotion
+							price={60}
+							oldPrice={90}
+							title="Air Max"
+							imageAlt="air-max"
+							imageURL="/images/airmax.jpg"
+						/>
+					</CarouselItem>
+
+					<CarouselItem className="basis-1/1 md:basis-1/3 lg:basis-1/5">
+						<ProductCard
+							promotion
+							price={50}
+							oldPrice={70}
+							title="Nike AJKO"
+							imageAlt="nike-ajko"
+							imageURL="/images/ajko.jpg"
+						/>
+					</CarouselItem>
+				</CarouselContent>
+			</Carousel>
 		</section>
 	)
 }
