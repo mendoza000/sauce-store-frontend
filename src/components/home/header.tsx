0ef300f912bd65122103733d47177c3b5701d581
mendoza000 <omarmendoza.alv25@gmail.com>
2025-09-24 13:43:45 -0400

    fix: Ajustar el diseño del encabezado y eliminar componente HeaderCategories

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,14 +1,9 @@
 import HeaderCarrousel from "./header-carrousel"
-import HeaderCategories from "./header-categories"
 import CurvedLoop from "@/components/CurvedLoop"
 
 export default function Header() {
 	return (
-		<header className="grid grid-cols-1 py-6 md:py-10 gap-5 px-1 sm:px-8 md:px-12 lg:px-20 bg-white">
-			{/* <div className="lg:col-span-1 md:col-span-2 hidden md:flex border-r-2 pr-5 border-zinc-300">
-				<HeaderCategories />
-			</div> */}
-
+		<header className="grid grid-cols-1 py-6 md:py-10 gap-5 px-1 sm:px-8 md:px-12 lg:px-10 xl:px-14 bg-white mt-14">
 			<HeaderCarrousel />
 
 			<CurvedLoop
